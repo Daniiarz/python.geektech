2c03f524119781c534c8770daba3ee465c8da190
SaltaK <s2103k@gmail.com>
2021-05-12 18:48:27 +0600

    homework4

---
+++ Home_Works/home_work_4/lesson1_4_dz.py
@@ -0,0 +1,34 @@
+todos = []
+
+
+while True:
+    action = input("Выберите действие: Y) Добавить задачу V) Показать задачи C) Отметить выполненные ")
+    if action == "Y":
+        todo = input("Задача: ")
+        deadline = input("Дэдлайн: ")
+        data = input("Добавлено: ")
+        completed = False
+        todos.append([todo, deadline, data, completed])
+        print("Задача добавлена!")
+    elif action == "V":
+        print()
+        if len(todos) == 0:
+            print("Нет задач!")
+        print("Задача       Дэдлайн       Дата добавления       Выполненные")
+        for pos, i in enumerate(todos):
+            print(pos, i[0], i[1], i[2], i[3])
+        print()
+    elif action == "С":
+        index = int(input("Отметить задачу "))
+        todos[index][3] = True
+        print(todos)
+    else:
+        print("Выберете правильный вариант!\n")
+
+
+
+
+
+
+
+
