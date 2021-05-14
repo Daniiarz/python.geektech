68d7d2e83b0821e3068998e73bcdb96096702ef9
SaltaK <s2103k@gmail.com>
2021-05-14 22:26:13 +0600

    homework5

--- Home_Works/home_work_4/lesson1_4_dz.py
+++ Home_Works/home_work_4/lesson1_4_dz.py
@@ -8,19 +8,19 @@ while True:
         deadline = input("Дэдлайн: ")
         data = input("Добавлено: ")
         completed = False
-        todos.append([todo, deadline, data, completed])
+        todos.append([todo, deadline, data, completed])   #список в списке
         print("Задача добавлена!")
     elif action == "V":
         print()
         if len(todos) == 0:
             print("Нет задач!")
         print("Задача       Дэдлайн       Дата добавления       Выполненные")
-        for pos, i in enumerate(todos):
+        for pos, i in enumerate(todos):    # позиция, нумерация
             print(pos, i[0], i[1], i[2], i[3])
         print()
     elif action == "С":
         index = int(input("Отметить задачу "))
-        todos[index][3] = True
+        todos[index][3] = True  # по индексу
         print(todos)
     else:
         print("Выберете правильный вариант!\n")
